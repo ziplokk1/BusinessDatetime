20223d0ed601ffc2f0064d2132444097b125a598
Mark <ziplokk.mark.sanders@gmail.com>
2016-06-07 13:50:21 -0500

    Added stuff for pip packaging

---
+++ README.md
@@ -0,0 +1,3 @@
+# Business Datetime
+
+This module includes a simple way to subtract two datetimes and get how many business years/months/days/hours/seconds/microseconds fall between the two dates IN WORKING HOURS.
---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+REQUIREMENTS = []
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+]
+
+setup(
+    name="python-business-datetime",
+    version=version,
+    description="A simple way to calculate working hours between two dates",
+    author="Mark Sanders",
+    author_email="sdscdeveloper@gmail.com",
+    url="http://github.com/ziplokk/BusinessDatetime",
+    packages=find_packages(),
+    platforms=['OS Independent'],
+    license='LICENSE.txt',
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    include_package_data=True,
+    zip_safe=False
+)
